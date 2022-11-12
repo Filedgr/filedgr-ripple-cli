e95c2e5178766c4aaaf23f9250a9ea4fd20f9abf
Esquive <e.falk@niugroup.lu>
2022-11-12 20:15:08 +0100

    added package config

--- src/filedgr_xrpl_cli/__main__.py
+++ src/filedgr_xrpl_cli/__main__.py
@@ -6,11 +6,12 @@ import orjson
 import typer
 from filedgr_nft_protobuf.serialize import serialize
 
+from filedgr_xrpl_cli.my_io.file_io import MyFileIO
 from nft_utils.id_gen import generate_nft_or_campaign_id
 from dto.network import NetworkChoices, all_networks
 from my_xrpl.connection import XRPLConnection
 from my_xrpl.tx import TransactionBuilder
-from my_xrpl.wallet import FileWalletLoader
+from my_xrpl.wallet import FileWalletLoader, XRPLWallet
 
 default_path = f"{Path.home()}/.filedger-xrpl-cli"
 main = typer.Typer()
@@ -36,9 +37,6 @@ def create_wallet(name: str,
                   dump: bool = True,
                   path: str = default_path,
                   network: NetworkChoices = "testnet") -> None:
-    from .my_xrpl.connection import XRPLConnection
-    from .my_xrpl.wallet import XRPLWallet
-    from .my_io.file_io import MyFileIO
 
     conn = XRPLConnection(json_rpc_url=all_networks.get(network.value).json_rpc_url)
     if network == NetworkChoices.testnet:
@@ -107,18 +105,20 @@ def set_trustline(issuer: str,
 
 @main.command("issue-nft")
 def issue_nft(issuer: str,
+              campaign_id: str,
+              nft_id: str,
               uri: str,
               path: str = default_path,
               network: NetworkChoices = "testnet"):
     issuer_wallet = FileWalletLoader().load_wallet(f"{path}/wallets/{issuer}")
     conn = XRPLConnection(json_rpc_url=all_networks.get(network.value).json_rpc_url)
 
-    nft_id = generate_nft_or_campaign_id()
-    campaign = generate_nft_or_campaign_id()
+    # nft_id = generate_nft_or_campaign_id()
+    # campaign = generate_nft_or_campaign_id()
 
     nft_uri = serialize(
         nft_id=nft_id,
-        campaign=campaign,
+        campaign=campaign_id,
         uri=uri
     )
 
@@ -151,6 +151,13 @@ def create_t_token(issuer: str,
         format=format
     )
     typer.echo(result)
+
+
+@main.command("generate-id")
+def generate_id():
+    nft_result = generate_nft_or_campaign_id()
+    campaign_result = generate_nft_or_campaign_id()
+    typer.echo(f"The NFT ID: {nft_result}; The campaign ID: {campaign_result}")
 
 
 if __name__ == '__main__':
